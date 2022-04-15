89657a0d2881ac1726c059735f84c356b40a711e
SamLee-dedeboy <583637704@qq.com>
2022-04-14 20:50:54 -0700

    fix panel size issuse

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,23 +18,33 @@ export default {
 
 <template>
   <main>
-    <Splitter layout="vertical" style="height:800px">
-      <SplitterPanel class="flex align-items-center justify-content-center" :size="10"> 
-        <MyToolbar/>
+    <Splitter style="height:100vh">
+      <SplitterPanel class="flex align-items-center justify-content-center" :size="80">
+        <Splitter layout="vertical" >
+          <SplitterPanel class="flex align-items-center justify-content-center" :size="10"> 
+            <MyToolbar/>
+          </SplitterPanel>
+          <SplitterPanel class="flex align-items-center justify-content-center" :size="90">
+              <Splitter layout="vertical" >
+                <SplitterPanel class="flex align-items-center justify-content-center" :size="10">
+                  <Filter></Filter>
+                </SplitterPanel>
+                <SplitterPanel class="flex align-items-center justify-content-center" :size="90">
+                  <Target></Target>
+                </SplitterPanel>
+              </Splitter>
+          </SplitterPanel>
+        </Splitter>
       </SplitterPanel>
-      <SplitterPanel class="flex align-items-center justify-content-center" :size="90">
-        <div>
-          <Splitter layout="vertical" style="height:720px">
-            <SplitterPanel class="flex align-items-center justify-content-center" :size="20">
-              <Filter></Filter>
-            </SplitterPanel>
-            <SplitterPanel class="flex align-items-center justify-content-center" :size="80">
-              <Target></Target>
-            </SplitterPanel>
-          </Splitter>
-        </div>
+      <SplitterPanel class="flex align-items-center justify-content-center" :size="20">
+        <p> side bar </p>
       </SplitterPanel>
     </Splitter>
-
   </main>
 </template>
+
+<style scoped>
+main {
+  max-width:1280px;
+}
+</style>
