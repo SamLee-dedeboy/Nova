18495c03debd7c45fbbb1b4d91ca8184dcdacfe7
SAM YUTE LEE <samytleezzz@gmail.com>
2022-04-24 15:01:23 -0700

    add Cloud-based Viz. Cloud-based and Graph-based Viz can be switched by changed TargetContainer child between "Target" and "Cloud".

--- src/components/MyToolbar.vue
+++ src/components/MyToolbar.vue
@@ -4,22 +4,28 @@ export default {
 emits: ["graph-dev"],
 methods: {
     testClicked() {
-        var outlet_set = ["CNN", "Fox", "NYT", "WSJ"]
+        var outlet_set = ["CNN", "Fox", "NYT", "WSJ", "ABC", "WP"]
         // original nodes and edges
         var graph1 = {
             nodes: [
-                {x: 25, y: 50, outlet:"CNN", sentiment: 0.8},
-                {x: 180, y: 80, outlet:"Fox", sentiment: -0.9},
-                {x: 20, y: 120, outlet:"NYT", sentiment: 0.1},
+                {x: 45, y: 70, outlet:"CNN", sentiment: -0.8},
+                {x: 180, y: 80, outlet:"Fox", sentiment: 0.9},
+                {x: 60, y: 120, outlet:"NYT", sentiment: -0.1},
+                {x: 170, y: 120, outlet:"ABC", sentiment: 0.5},
+                {x: 200, y: 100, outlet:"WP", sentiment: 0.1}
+
             ],
             center_node : {x: 70, y: 80, text:"Joe Biden"}
         }
 
         var graph2 = {
             nodes: [
-                {x: 25, y: 30, outlet:"CNN", sentiment: 0.1},
-                {x: 10, y: 80, outlet:"Fox", sentiment: 0.9},
-                {x: 150, y: 100, outlet:"WSJ", sentiment: -0.8},
+                {x: 60, y: 70, outlet:"CNN", sentiment: -0.1},
+                {x: 50, y: 110, outlet:"Fox", sentiment: -0.9},
+                {x: 160, y: 110, outlet:"WSJ", sentiment: 0.8},
+                {x: 150, y: 70, outlet:"ABC", sentiment: 0.5},
+                {x: 185, y: 80, outlet:"WP", sentiment: 0.2}
+
             ],
             center_node : {x: 80, y: 80, text:"BLM"}
         }
@@ -30,7 +36,7 @@ methods: {
             var dotted_nodes = []
             outlet_set.forEach(outlet => {
             if(!graph.nodes.map(a => a.outlet).includes(outlet)) {
-                dotted_nodes.push({x:100, y:20, outlet:outlet, sentiment:0, dotted:true})
+                dotted_nodes.push({x:115, y:30, outlet:outlet, sentiment:0, dotted:true})
             } 
         })
             graph.nodes.push.apply(graph.nodes, dotted_nodes)
