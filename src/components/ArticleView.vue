6c7fccc9597b66c0bef0e47f84853ed3d12e1259
SAM YUTE LEE <samytleezzz@gmail.com>
2022-04-25 16:58:42 -0700

    Add toggle feature on whole header element Default <Panel> only supports toggling on button. This commit manually handles toggling when clicked on panel header.

--- src/components/ArticleView.vue
+++ src/components/ArticleView.vue
@@ -2,6 +2,8 @@
 import { defineComponent } from '@vue/composition-api'
 import ScrollPanel from 'primevue/scrollpanel'
 import Panel from 'primevue/panel'
+import { nextTick } from 'vue'
+
 export default defineComponent({
     setup() {
         
@@ -11,17 +13,38 @@ export default defineComponent({
     Panel,
       ScrollPanel
     },
+    watch: {
+        articles: async function() {
+            await nextTick();
+            var headers = document.getElementsByClassName('p-panel-header');
+            for (let header of headers) {
+                header.onclick = function(e) {
+                    if(e.target != header.childNodes[3].childNodes[2] &&
+                        ![...header.childNodes[3].childNodes[2].childNodes].includes(e.target)
+                    ) 
+                        header.childNodes[3].childNodes[2].click()
+                }
+            }
+
+        
+        }
+    },
 })
 </script>
 <template>
     <Panel v-for="(article, index) in articles"
     :key="article.id"
     :header="index+1 + '. ' + article.headline"
-    toggleable="true"
-    collapsed="false"
+    :toggleable=true
+    :collapsed=true
     >
     <ScrollPanel style="width: 100%; height: 200px">
         {{article.content}}
     </ScrollPanel>
     </Panel>
 </template>
+<style scoped>
+::v-deep .p-panel-header {
+    cursor: pointer;
+}
+</style>
