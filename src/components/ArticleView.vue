a09e5aae3729babbd98ca963075d3fa43e944b91
SAM YUTE LEE <samytleezzz@gmail.com>
2022-04-26 17:19:09 -0700

    Add background color in ArticleView background color is determined by article's sentiment score

--- src/components/ArticleView.vue
+++ src/components/ArticleView.vue
@@ -3,6 +3,7 @@ import { defineComponent } from '@vue/composition-api'
 import ScrollPanel from 'primevue/scrollpanel'
 import Panel from 'primevue/panel'
 import { nextTick } from 'vue'
+import * as d3 from "d3";
 
 export default defineComponent({
     setup() {
@@ -13,11 +14,23 @@ export default defineComponent({
     Panel,
       ScrollPanel
     },
+    
+    mounted() {
+        this.color_neg = d3.scaleSqrt()
+            .domain([-1, 0])  
+            .range(["#F53241", "#F3BBBF"]); 
+        this.color_pos = d3.scaleSqrt()
+            .domain([0, 1])
+            .range(["khaki", "yellow"]);
+    },
     watch: {
         articles: async function() {
             await nextTick();
             var headers = document.getElementsByClassName('p-panel-header');
+            var i = 0;
             for (let header of headers) {
+                header.setAttribute("style", "background-color:" + this.color_neg(this.articles[i].sentiment))
+                i++
                 header.onclick = function(e) {
                     if(e.target != header.childNodes[3].childNodes[2] &&
                         ![...header.childNodes[3].childNodes[2].childNodes].includes(e.target)
@@ -33,8 +46,8 @@ export default defineComponent({
 </script>
 <template>
     <Panel v-for="(article, index) in articles"
-    :key="article.id"
     :header="index+1 + '. ' + article.headline"
+    :key="article.id"
     :toggleable=true
     :collapsed=true
     >
