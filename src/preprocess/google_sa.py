64ae63af09019fc91e7605ec02b58c049f7ec63c
SamLee-dedeboy <583637704@qq.com>
2022-05-25 16:18:09 -0700

    add codes for getting entities using google api

--- src/preprocess/google_sa.py
+++ src/preprocess/google_sa.py
@@ -71,3 +71,37 @@ def analyze_entity_sentiment(sentences):
         #     print(
         #         u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
         #     )
+def analyze_entity(sentences):
+    text_content = " ".join(sentences)
+    client = language_v1.LanguageServiceClient()
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type})
+    return list(filter(lambda entity: "wikipedia_url" in entity.metadata.keys(), response.entities))
+    # print(response.entities)
+    # Loop through entitites returned from the API
+    # for entity in response.entities:
+    #     print("---------------")
+    #     print(u"Representative name for the entity: {}".format(entity.name))
+    #     # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+    #     print(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
+    #     # Get the salience score associated with the entity in the [0, 1.0] range
+    #     print(u"Salience score: {}".format(entity.salience))
+    #     # Loop over the metadata associated with entity. For many known entities,
+    #     # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+    #     # Some entity types may have additional metadata, e.g. ADDRESS entities
+    #     # may have metadata for the address street_name, postal_code, et al.
+    #     for metadata_name, metadata_value in entity.metadata.items():
+    #         print(u"{}: {}".format(metadata_name, metadata_value))
+
+    #     # Loop over the mentions of this entity in the input document.
+    #     # The API currently supports proper noun mentions.
+    #     for mention in entity.mentions:
+    #         print(u"Mention text: {}".format(mention.text.content))
+    #         # Get the mention type, e.g. PROPER for proper noun
+    #         print(
+    #             u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
+    #         )
