ea8c84e1739aeb0bd9a9b86cfb4a442a95f4b1f9
SAM YUTE LEE <samytleezzz@gmail.com>
2022-05-30 16:15:22 -0700

    add batches to entity_link.py

--- src/preprocess/entity_link.py
+++ src/preprocess/entity_link.py
@@ -1,4 +1,5 @@
 import time
+import numpy as np 
 import preprocess
 from collections import defaultdict
 import swat_ned
@@ -8,7 +9,7 @@ dirname = os.path.dirname(__file__)
 relative_path = os.path.join(dirname, "rel_ned")
 sys.path.insert(0, relative_path) 
 from ned_utils import REL_NedAnalyzer
-
+import preprocess
 def add_entity(src_dataset_path, dst_dataset_path, entity_list_path, subset=None):
     # init
     start_time = time.time()
@@ -17,35 +18,46 @@ def add_entity(src_dataset_path, dst_dataset_path, entity_list_path, subset=None
     entitiesToArticle_dict = defaultdict(list)
     # urlToName_dict = defaultdict(set)
     articles = dataset if subset == None else dataset[0:subset]
+    
+    # split dataset in to batches
+    batch_size = 100
+    article_batches = np.array_split(articles, int(len(articles)/batch_size))
 
-    # process entities
-    article_entity_dict = rel_analyzer.analyze_entity(articles)
-    for article in articles:
-        entity_list = article_entity_dict[article["id"]]
-        article["entities"] = entity_list
-        for entity in entity_list:
-            entitiesToArticle_dict[entity].append(article["id"])
-        # google entities
-        # google_entities = google_sa.analyze_entity(sentences)
-        # for entity in google_entities:
-        #     entitiesToArticle_dict[entity.metadata["wikipedia_url"]].append(article["id"])
-        #     urlToName_dict[entity.metadata["wikipedia_url"]].add(entity.name)
-        # article["entities"] = [next(iter((urlToName_dict[entity.metadata["wikipedia_url"]]))) for entity in google_entities] 
+    for (batch_index,batch) in enumerate(article_batches):
+        # process entities
+        article_entity_dict = rel_analyzer.analyze_entity(batch, lambda content: " ".join(preprocess.article_to_sentences(content)))
+        print("-------------------------------")
+        print("Batch {} Analyze Done! Post-processing...".format(batch_index))
+        print("-------------------------------")
+        for article in batch:
+            entity_list = article_entity_dict[article["id"]]
+            article["entities"] = entity_list
+            for entity in entity_list:
+                entitiesToArticle_dict[entity].append(article["id"])
+            # google entities
+            # google_entities = google_sa.analyze_entity(sentences)
+            # for entity in google_entities:
+            #     entitiesToArticle_dict[entity.metadata["wikipedia_url"]].append(article["id"])
+            #     urlToName_dict[entity.metadata["wikipedia_url"]].add(entity.name)
+            # article["entities"] = [next(iter((urlToName_dict[entity.metadata["wikipedia_url"]]))) for entity in google_entities] 
 
-        # swat entities
-        # swat_entities, entityIdToName_dict = swat_ned.getEntityList(sentences, article["headline"])
-        # for entityId in swat_entities:
-        #     entitiesToArticle_dict[entityId].append(article["id"])
-        #     urlToName_dict[entityId].update(entityIdToName_dict[entityId])
-        # article["entities"] = [next(iter((urlToName_dict[entityId]))) for entityId in swat_entities]
+            # swat entities
+            # swat_entities, entityIdToName_dict = swat_ned.getEntityList(sentences, article["headline"])
+            # for entityId in swat_entities:
+            #     entitiesToArticle_dict[entityId].append(article["id"])
+            #     urlToName_dict[entityId].update(entityIdToName_dict[entityId])
+            # article["entities"] = [next(iter((urlToName_dict[entityId]))) for entityId in swat_entities]
 
-        # REL
-        # rel_entities = rel_analyzer.analyze_entity(article)
-        # for entity in rel_entities:
-        #     entitiesToArticle_dict[entity].append(article["id"])
-        # article["entities"] = rel_entities
+            # REL
+            # rel_entities = rel_analyzer.analyze_entity(article)
+            # for entity in rel_entities:
+            #     entitiesToArticle_dict[entity].append(article["id"])
+            # article["entities"] = rel_entities
 
     # urlToName_dict = {k: list(v) for k, v in urlToName_dict.items()}
+    print("-------------------------------")
+    print("Entity Processing Done! Saving....")
+    print("-------------------------------")
     preprocess.dict_to_json(entitiesToArticle_dict, filepath=entity_list_path)
     preprocess.dict_to_json(articles, filepath=dst_dataset_path)
     # preprocess.dict_to_json(urlToName_dict, filepath="data/WikiIDToEntityName.json")
