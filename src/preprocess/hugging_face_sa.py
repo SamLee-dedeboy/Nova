d1106c44e639cc4b8419047ab903010f03eb4dab
SAM YUTE LEE <samytleezzz@gmail.com>
2022-05-30 16:16:06 -0700

    add exception handler in sentiment analysis to prevent breaking

--- src/preprocess/hugging_face_sa.py
+++ src/preprocess/hugging_face_sa.py
@@ -11,16 +11,21 @@ class hugFace:
         pos_sentences = 0
         neg_sentences = 0
         for sentence in sentences:
-            sst = classifier(sentence)[0] 
-            sent_binary = 1 if sst["label"] == "POSITIVE" else -1   
-            score = sent_binary * float(sst["score"])
-            if score > 0:
-                sst_pos += score
-                pos_sentences += 1
-            else:
-                sst_neg += score
-                neg_sentences += 1
+            try:
+                sst = classifier(sentence)[0] 
+                sent_binary = 1 if sst["label"] == "POSITIVE" else -1   
+                score = sent_binary * float(sst["score"])
+                if score > 0:
+                    sst_pos += score
+                    pos_sentences += 1
+                else:
+                    sst_neg += score
+                    neg_sentences += 1
+            except:
+                print("sentence token length too long! Considered noise.")
 
+
+        
         return  {"pos": sst_pos, "neg": sst_neg, "pos_sentences": pos_sentences, "neg_sentences": neg_sentences} 
         
 
