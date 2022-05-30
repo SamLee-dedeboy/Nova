d92bd80f1d6cc0db9754c12b9cd632b3bada129e
SAM YUTE LEE <samytleezzz@gmail.com>
2022-05-30 16:17:26 -0700

    change subset into subset_index'

--- src/preprocess/process_script.py
+++ src/preprocess/process_script.py
@@ -2,19 +2,24 @@ import time
 import entity_link
 import sentiment
 
+subset_index = None
 def main():
     start_time = time.time()
-    entity_link.add_entity(
-        src_dataset_path="data/single_topic_articles.json",
-        dst_dataset_path="data/processed_articles_rel.json",
-        entity_list_path="data/rel_entities.json",
-        subset=1000
-        )
+    # entity_link.add_entity(
+    #     src_dataset_path="data/single_topic_articles.json",
+    #     dst_dataset_path="data/processed_articles_rel.json",
+    #     entity_list_path="data/rel_entities.json",
+    #     subset=subset_index
+    #     )
     entity_time = time.time() - start_time
+    print("--------------------------")
+    print("entity extraction done!")
+    print("time used: {}".format(entity_time))
+    print("--------------------------")
     sentiment.add_sentiment(
         src_dataset_path="data/processed_articles_rel.json",
         dst_dataset_path="data/processed_articles_hugFace.json",
-        subset=1000
+        subset=subset_index
     )
     sentiment_time = time.time() - entity_time
     print("--- entity time: {} ---".format(entity_time))
