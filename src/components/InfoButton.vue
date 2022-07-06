a69effffa01cd52a4ffd547340aaddea301146f2
SamLee-dedeboy <583637704@qq.com>
2022-07-06 14:06:53 -0700

    Separate InfoButton as a component

---
+++ src/components/InfoButton.vue
@@ -0,0 +1,27 @@
+<script>
+import Tooltip from './Tooltip.vue';
+export default {
+    components: {
+        Tooltip,
+    },
+    props:["info_content"],
+}
+</script>
+
+<template>
+    <div class="info-button-container">
+        <Button 
+        class="p-button-rounded p-button-outlined p-button-text"
+        icon="pi pi-info-circle"
+        @mouseover="() => this.$refs.entity_tooltip.show()"
+        @mouseleave="() => this.$refs.entity_tooltip.hide()"
+        ></Button>
+        <Tooltip 
+        ref="entity_tooltip"
+        :content="info_content"
+        style="font-size:smaller; "></Tooltip>
+    </div>
+</template>
+<style>
+
+</style>
