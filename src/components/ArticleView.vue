025916a43e21039e6fe3fa8556660c99765ea41f
SamLee-dedeboy <583637704@qq.com>
2022-09-13 18:41:21 -0700

    add dynamic rendering to article view

--- src/components/ArticleView.vue
+++ src/components/ArticleView.vue
@@ -2,6 +2,7 @@
 import ScrollPanel from 'primevue/scrollpanel'
 import Panel from 'primevue/panel'
 import Chip from 'primevue/chip'
+import DeferreedContent from 'primevue/deferredcontent'
 import ToggleButton from 'primevue/togglebutton'
 import { nextTick } from 'vue'
 import * as d3 from "d3";
@@ -26,6 +27,28 @@ const props = defineProps({
 
 const emit = defineEmits(["update:sst_threshold"])
 
+vue.onMounted(() => {
+    const pos_scroll_panel = document.querySelector(".pos-article-list > .p-scrollpanel-wrapper > .p-scrollpanel-content")
+    const neg_scroll_panel = document.querySelector(".neg-article-list > .p-scrollpanel-wrapper > .p-scrollpanel-content")
+    pos_scroll_panel?.addEventListener("scroll", function(event) {
+        const element = event.target as HTMLElement
+        if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+            const old_len = pos_panel_articles.value.length
+            const next_ten_articles: Article[] | undefined = pos_articles.value?.slice(old_len, old_len+10)
+            if(next_ten_articles)
+                pos_panel_articles.value = pos_panel_articles.value.concat(next_ten_articles)
+        }
+    })
+    neg_scroll_panel?.addEventListener("scroll", function(event) {
+        const element = event.target as HTMLElement
+        if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+            const old_len = neg_panel_articles.value.length
+            const next_ten_articles: Article[] | undefined = neg_articles.value?.slice(old_len, old_len+10)
+            if(next_ten_articles)
+                neg_panel_articles.value = neg_panel_articles.value.concat(next_ten_articles)
+        }
+    })
+})
 const sorted_articles = vue.computed(() => {
     return props.articles?.sort((a1, a2) => {
         const s1 = a1.sentiment.score || 0
@@ -36,9 +59,11 @@ const sorted_articles = vue.computed(() => {
 const pos_articles = vue.computed(() => {
     return props.articles?.filter(article => article.sentiment.label === "POSITIVE")
 })
+const pos_panel_articles: Ref<Article[]> = ref(pos_articles.value?.splice(0,10) || []) 
 const neg_articles = vue.computed(() => {
     return props.articles?.filter(article => article.sentiment.label === "NEGATIVE")
 })
+const neg_panel_articles: Ref<Article[]> = ref(pos_articles.value?.splice(0,10) || []) 
 
 function sstToColor(sst: number) {
     if(isNeutral(sst)) return SstColors.neu_color
@@ -67,8 +92,9 @@ function adjustThreshold(article) {
 </script>
 
 <template>
+<DeferredContent>
 <ScrollPanel class="pos-article-list">
-    <Panel v-for="(article, index) in pos_articles"
+    <Panel v-for="(article, index) in pos_panel_articles"
     :header="index+1 + '. ' + article.headline"
     :key="article.id"
     :toggleable=true
@@ -89,7 +115,7 @@ function adjustThreshold(article) {
 </ScrollPanel>
 
 <ScrollPanel class="neg-article-list">
-    <Panel v-for="(article, index) in neg_articles"
+    <Panel v-for="(article, index) in neg_panel_articles"
     :header="index+1 + '. ' + article.headline"
     :key="article.id"
     :toggleable=true
@@ -108,6 +134,7 @@ function adjustThreshold(article) {
     </ScrollPanel>
     </Panel>
 </ScrollPanel>
+</DeferredContent>
 </template>
 <style scoped>
 :deep(.p-panel-header) {
