b4838418c5705a53d7de72c79487e28127a46c3d
SamLee-dedeboy <583637704@qq.com>
2022-10-06 00:33:51 -0700

    add component header & tooltip css to App.vue to use globally

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,70 @@ header {
 main {
   min-height: 100vh;
 }
+
+.component-header {
+  margin: 2%;
+  border-bottom: solid 1px #b7b7b7;
+  font-family: 'Lato';
+  font-weight: bold;
+}
+.tooltip .tooltiptext {
+  visibility: hidden;
+  background-color: white;
+  text-align: center;
+  padding: 5px 0;
+  border-radius: 6px;
+  border: solid 1px black;
+  /* Position the tooltip text */
+  position: absolute;
+  z-index: 2;
+  /* Fade in tooltip */
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+.tooltip .top-tooltiptext {
+  width: 120px;
+  margin-left: -60px;
+  bottom: 100%;
+  left: 50%;
+}
+
+.tooltip .right-tooltiptext {
+  left: 125%;
+}
+
+/* .tooltip .top-tooltiptext::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+  z-index: 100;
+} */
+
+/* .tooltip .right-tooltiptext::after {
+  content: " ";
+  position: absolute;
+  top: 15%;
+  right: 100%; 
+  margin-top: -5px;
+  border-width: 5px;
+  border-style: solid;
+  border-color: transparent black transparent transparent;
+} */
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
+
+a {
+  font-family: "Lato";
+}
 /* 
 @media (min-width: 1024px) {
 
