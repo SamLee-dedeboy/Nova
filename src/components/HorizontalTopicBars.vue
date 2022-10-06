fc80a537077b488272a8be179a6510a41dc8f89a
SamLee-dedeboy <583637704@qq.com>
2022-10-06 00:34:30 -0700

    Add Horizontal Bars & change TopicBar color

--- src/components/HorizontalTopicBars.vue
+++ src/components/HorizontalTopicBars.vue
@@ -7,8 +7,8 @@ const props = defineProps({
     targetTopicBins: Object as () => { [id: string]: {pos: number, neg: number}},
     cooccurrTopicBins: Object as () => { [id: string]: {pos: number, neg: number}},
 })
-const viewBox = [550, 400]
-const margin = {top: 30, bottom: 20, right:10, left: 50, middle: 10} 
+const viewBox = [750, 400]
+const margin = {top: 30, bottom: 20, right:10, left: 150, middle: 10} 
 const viewBox_width = viewBox[0] - margin.left - margin.right
 const viewBox_height = viewBox[1] - margin.top - margin.bottom - margin.middle
 const max_articles = vue.computed(() => {
@@ -49,24 +49,24 @@ vue.onMounted(() => {
     svg.append("g")
         .attr("class", "canvas")
         .attr("transform", `translate(${margin.left}, ${margin.top})`)
-    // x axis
-    x = d3.scaleBand()
-        .range([ 0, viewBox_width ])
-        .domain(Object.keys(props.targetTopicBins!).sort())
-        .padding(0.2);
     const canvas = svg.select("g.canvas")
+    // x axis
+    x = d3.scaleLinear()
+        .domain([0, max_articles.value])
+        .range([0, viewBox_width])
     canvas.append("g").attr("class", "x-axis-group")
         .attr("transform", `translate(0, ${viewBox_height})`)
-        .style("font-size", "1.1rem")
-        .call(d3.axisBottom(x).tickFormat((d: any) => abbr_dict[d]))
-    // y axis
-    y = d3.scaleLinear()
-        .domain([0, max_articles.value])
-        .range([viewBox_height, 0])
-    canvas.append("g").attr("class", "y-axis-group")
         .style("font-size", "1rem")
-        .call(d3.axisLeft(y))
+        .call(d3.axisBottom(x))
     
+    // y axis
+    y = d3.scaleBand()
+        .domain(Object.keys(props.targetTopicBins!).sort())
+        .range([ 0, viewBox_height ])
+        .padding(0.2);
+    canvas.append("g").attr("class", "y-axis-group")
+        .style("font-size", "1.25rem")
+        .call(d3.axisLeft(y).tickFormat((d: any) => abbr_dict[d]))
     canvas.append("g").attr("class", "bars-group")
     updateBars()
 })
@@ -84,11 +84,11 @@ function updateBars() {
         .attr("class", "target")
         .attr("y", y(0))
         .merge(target_rects)
-            .attr("width", (d) => x.bandwidth())
-            .attr("x", (d: any) => x(d.topic) + 0)
+            .attr("height", (d) => y.bandwidth())
+            .attr("x", (d) => 0)
+            .attr("y", (d: any) => y(d.topic) + 0)
             .transition().duration(1000)
-            .attr("y", (d) => y(d.num))
-            .attr("height", (d) => viewBox_height-y(d.num))
+            .attr("width", (d) => x(d.num))
             .attr("fill", "white")
             .attr("stroke", "black")
             .style("filter", `brightness(${SstColors.brightness}%)`)
@@ -99,17 +99,21 @@ function updateBars() {
             .attr("class", "cooccurr")
             .attr("y", y(0))
             .merge(cooccurr_rects)
-                .attr("width", (d) => x.bandwidth())
+                .attr("height", (d) => y.bandwidth()-1)
                 // .attr("x", (d: any) => x(d.topic) + x.bandwidth()/bar_num.value)
-                .attr("x", (d: any) => x(d.topic) + 0)
+                .attr("x", (d) => 1)
+                .attr("y", (d: any) => y(d.topic) + 0 - 1)
                 .transition().duration(1000)
-                .attr("y", (d) => y(d.num))
-                .attr("height", (d) => viewBox_height-y(d.num))
+                .attr("width", (d) => x(d.num))
                 .attr("fill", SstColors.topic_bar_color)
                 .style("filter", `brightness(${SstColors.brightness}%)`)
     } else {
         bars_group.selectAll("g.topic").selectAll("rect.cooccurr").remove()
     }
+
+
+
+
 }
 </script>
 <template>
@@ -120,10 +124,9 @@ function updateBars() {
 <style scoped>
 .topic-barchart-container {
     height: 100%;
-    width: 100%;
+    width: 59%;
 }
 .topic-barchart {
-    height: 100%;
-    width: 100%;
+    height: 100%
 }
 </style>
