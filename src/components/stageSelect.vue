b498663b8b82901ddeb4988713b9700b95da1269
Keshav Dasu <keshavdasu@gmail.com>
2023-02-13 15:21:34 -0800

    Steps for breadcrumb

---
+++ src/components/stageSelect.vue
@@ -0,0 +1,27 @@
+<template>
+    <Steps :modal="stages" />
+</template>
+
+<script setup lang="ts">
+import Steps from 'primevue/steps';
+
+const stages = [
+    {
+        label: 'Topic Selection',
+        to: '/'
+    },
+    {
+        label: 'Compare',
+        to: '/compare/:entity/'
+    },
+    {
+        label: 'Article Inspection',
+        to: '/inspection/:entity'
+    }
+]
+</script>
+
+
+<style>
+
+</style>
