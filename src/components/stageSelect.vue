0a1cb7d3f941a364c9950add4d7cebecd7dde779
SamLee-dedeboy <samytleezzz@gmail.com>
2023-02-14 14:42:59 -0800

    add step disabling

--- src/components/stageSelect.vue
+++ src/components/stageSelect.vue
@@ -1,5 +1,5 @@
 <template>
-    <Steps :model="stages" :readonly="enableNext" aria-label="Form Steps"/>
+    <Steps :model="stages" :readonly="false" aria-label="Form Steps"/>
 </template>
 
 <script setup lang="ts">
@@ -13,34 +13,41 @@ const store = useUserDataStore()
 const route = useRoute()
 const selected_entity = vue.computed(() => store.selected_entity)
 const random_outlet = vue.inject("random_outlet")
-const enableNext = vue.computed(() => {
-    console.log(route.name)
+
+const disabled_item_array = vue.computed(() => {
     switch (route.name) {
-        case "home": return selected_entity.value == undefined
-        case "belief": return true
-        default: return false;
+        case "home": return [true, selected_entity.value == undefined, true, true, true]
+        case "belief": return [false, false, true, true, true]
+        case "compare": return [false, false, false, false, false]
+        case "inspection": return [false, false, false, false, false]
+        default: return [true, true, true, true, true]
     }
 })
 const stages = vue.computed(() => [
     {
       to: `/`,
       label: "Home",
+      disabled: disabled_item_array.value[0],
     },
     {
       to: `/belief/first/${random_outlet[0]}/${selected_entity.value?.name}`,
       label: "Belief",
+      disabled: disabled_item_array.value[1],
     },
     {
-      to: `/belief/secoond/${random_outlet[1]}/${selected_entity.value?.name}`,
+      to: `/belief/second/${random_outlet[1]}/${selected_entity.value?.name}`,
       label: "Belief",
+      disabled: disabled_item_array.value[2],
     },
     {
       to: `/compare/${selected_entity.value?.name}/`,
       label: "Compare",
+      disabled: disabled_item_array.value[3],
     },
     {
       to: `/inspection/${selected_entity.value?.name}`,
       label: "Inspection",
+      disabled: disabled_item_array.value[4],
     },
     // {
     //   to: `/summary/${selected_entity.value?.name}`,
